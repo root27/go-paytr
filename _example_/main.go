910b51c2e4015a59b3707c5fd5b566234cfeab29
root27 <oguzhaneee@gmail.com>
2024-05-29 09:53:24 +0300

    callback added

--- _example_/main.go
+++ _example_/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/root27/go-paytr"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/schema"
+	"github.com/root27/go-paytr"
 )
 
 var (
@@ -16,6 +18,8 @@ func main() {
 	r := http.NewServeMux()
 
 	r.HandleFunc("/payment", handlePayment)
+
+	r.HandleFunc("/paymentCallback", handleCallback)
 
 	log.Println("Test server is running port ", PORT)
 
@@ -84,8 +88,62 @@ func handlePayment(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
-
+	//TODO: Return iframe token to client
 	log.Println(token)
+}
+
+//NOTE: Paytr Callback API
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+
+	if err != nil {
+
+		log.Printf("Error parsing request form:%s\n", err.Error())
+
+		http.Error(w, "Bad request", http.StatusBadRequest)
+
+		return
+
+	}
+
+	decoder := schema.NewDecoder()
+
+	var payment paytr.CallbackRequest
+
+	err = decoder.Decode(&payment, r.PostForm)
+
+	if err != nil {
+
+		log.Printf("Error decoding postform:%s\n", err.Error())
+
+		http.Error(w, "Error decoding postform", http.StatusInternalServerError)
+
+		return
+	}
+
+	valid := payment.IsValid(merchantKey, merchantSalt)
+
+	if !valid {
+
+		return
+
+	}
+
+	if payment.Status != "success" {
+
+		log.Printf("Error payment process\n")
+
+		w.Write([]byte("OK"))
+
+		return
+	}
+
+	//NOTE:Payment Successfull
+
+	w.Write([]byte("OK"))
+
 }
 
 type Request struct {
